dc496e4a7328dd704c62fa06ed463d08e7d22c52
seanswlee <lee.sw.sean@gmail.com>
2022-04-05 16:39:19 -0700

    Comments added for error handling

--- errors.mjs
+++ errors.mjs
@@ -1,11 +1,13 @@
 import { readFile } from 'fs/promises';
 
+// Catching errors with control
 // try {
 //   const result = await readFile(new URL('app.mjs', import.meta.url), 'utf-8');
 // } catch (err) {
 //   throw err;
 // }
 
+// Catching errors when you do not have control over processes
 process.on('uncaughtException', (err) => {
   console.error(err);
 });
